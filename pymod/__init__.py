387707d9e58c3769ecde99d5adf9470ad79a680c
Gabriel Studer <gabriel.studer@unibas.ch>
2016-01-31 12:19:57 +0100

    make it possible to directly import the objects defined in PYTHON

--- pymod/__init__.py
+++ pymod/__init__.py
@@ -1,2 +1,6 @@
 from ost import *
 from _qmean import *
+from qmean.predicted_sequence_features import PSIPREDHandler
+from qmean.predicted_sequence_features import ACCPROHandler
+from qmean.score_calculator import LocalScorer
+from qmean.score_calculator import GlobalScorer
