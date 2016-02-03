eunos-1128/QMEAN

commit 302180e2a835ba35a7069d7226f36c6e534e41e6

do a lot of parameter checks to prevent segfault cause by weird parametrization

src/impl/cbeta_impl.cc
@@ -50,7 +50,7 @@ void CBetaPotentialImpl::SetEnvironment(ost::mol::EntityView& env){
   }
 }
 
-bool CBetaPotentialImpl::VisitResidue(const ost::mol::ResidueHandle& res){
+bool CBetaPotentialImpl::VisitResidue(ost::mol::ResidueHandle& res){
 
   if (!res.IsPeptideLinking()) {
     return false;

src/impl/cbeta_impl.hh
@@ -16,7 +16,19 @@ CBetaOpts() { }
 CBetaOpts(Real l_cutoff, Real u_cutoff, int nob, int ssep, Real s=0.02):
           lower_cutoff(l_cutoff), upper_cutoff(u_cutoff), number_of_bins(nob),
           sequence_sep(ssep), sigma(s)
-  { }
+  { 
+    if(l_cutoff >= u_cutoff){
+      throw std::runtime_error("Lower cutoff must be smaller than upper cutoff!");
+    }
+
+    if(l_cutoff < 0.0){
+      throw std::runtime_error("Lower cutoff must be greater or equal 0.0!");
+    }
+
+    if(s <= 0.0){
+      throw std::runtime_error("Sigma must be larger larger than 0.0!");
+    }
+  }
 
 public:
   Real lower_cutoff;
@@ -66,7 +78,7 @@ public:
     opts_(opts), env_(5) { }
 
 
-  virtual bool VisitResidue(const ost::mol::ResidueHandle& res);
+  virtual bool VisitResidue(ost::mol::ResidueHandle& res);
 
   virtual void OnInteraction(ost::conop::AminoAcid a,
                              ost::conop::AminoAcid b,

src/cb_packing_potential.cc
@@ -88,6 +88,11 @@ void CBPackingPotential::Fill(CBPackingStatisticPtr stat, const String& referenc
 }
 
 Real CBPackingPotential::GetEnergy(ost::conop::AminoAcid aa, int count){
+
+  if(aa == ost::conop::XXX){
+    throw std::runtime_error("Cannot get energy for invalid amino acid!");
+  }
+
   return energies_.Get(CBPackingEnergies::IndexType(aa, this->GetBin(count)));
 }
 

src/cb_packing_potential.hh
@@ -30,7 +30,7 @@ public:
 
   void Fill(CBPackingStatisticPtr s1, CBPackingStatisticPtr s2, const String& reference_state, Real max_energy);
 
-  PotentialType GetType() { return CBPacking; }
+  PotentialType GetType() const { return CBPacking; }
 
   Real GetEnergy(ost::conop::AminoAcid aa, int count);
 
@@ -42,7 +42,7 @@ public:
 
   Real GetTotalEnergy(ost::mol::EntityView& target, ost::mol::EntityView& env, bool normalize);
 
-  impl::CBPackingOpts GetOpts() { return opts_; }
+  impl::CBPackingOpts GetOpts() const { return opts_; }
 
   template <typename DS>
   void Serialize(DS& ds){

src/cb_packing_statistics.cc
@@ -44,7 +44,7 @@ void CBPackingStatistic::Extract(ost::mol::EntityView& target, ost::mol::EntityV
   target.Apply(*this);
 }
 
-Real CBPackingStatistic::GetTotalCount(){
+Real CBPackingStatistic::GetTotalCount() const{
   Real total_count=0;
   for(int aa=0;aa<ost::conop::XXX;++aa){
     total_count+=this->GetCount(ost::conop::AminoAcid(aa));
@@ -52,7 +52,7 @@ Real CBPackingStatistic::GetTotalCount(){
   return total_count;
 }
 
-Real CBPackingStatistic::GetCount(ost::conop::AminoAcid aa){
+Real CBPackingStatistic::GetCount(ost::conop::AminoAcid aa) const{
 
   Real total_count=0;
   for(int i=0;i<opts_.max_counts+1;++i){
@@ -61,11 +61,20 @@ Real CBPackingStatistic::GetCount(ost::conop::AminoAcid aa){
   return total_count;
 }
 
-Real CBPackingStatistic::GetCount(ost::conop::AminoAcid aa, int bin){
+Real CBPackingStatistic::GetCount(ost::conop::AminoAcid aa, uint bin) const{
+
+  if(aa == ost::conop::XXX){
+    throw std::runtime_error("Cannot get count for invalid amino acid!");
+  }
+
+  if(bin >= opts_.max_counts+1){
+    throw std::runtime_error("Cannot get count for invalid bin!");
+  }
+
   return histo_.Get(CBPackingHistogram::IndexType(aa,bin));
 }
 
-Real CBPackingStatistic::GetCount(int bin){
+Real CBPackingStatistic::GetCount(uint bin) const{
 
   Real total_count=0;
   for(int i=0; i < ost::conop::XXX; ++i){

src/cb_packing_statistics.hh
@@ -28,17 +28,17 @@ public:
 
   void Extract(ost::mol::EntityView& target, ost::mol::EntityView& env, Real weight);
 
-  Real GetTotalCount();
+  Real GetTotalCount() const;
 
-  Real GetCount(ost::conop::AminoAcid aa);
+  Real GetCount(ost::conop::AminoAcid aa) const;
 
-  Real GetCount(ost::conop::AminoAcid aa, int bin);
+  Real GetCount(ost::conop::AminoAcid aa, uint bin) const;
 
-  Real GetCount(int bin);
+  Real GetCount(uint bin) const;
 
-  PotentialType GetType() { return CBPacking; }
+  PotentialType GetType() const { return CBPacking; }
 
-  impl::CBPackingOpts GetOpts() { return opts_; }
+  impl::CBPackingOpts GetOpts() const { return opts_; }
 
   template <typename DS>
   void Serialize(DS& ds){

src/cbeta_potential.hh
@@ -32,7 +32,7 @@ public:
 
   void Fill(CBetaStatisticPtr s1, CBetaStatisticPtr s2, const String& reference_state, Real max_energy);
 
-  PotentialType GetType() { return CBeta; }
+  PotentialType GetType() const { return CBeta; }
 
   Real GetEnergy(ost::conop::AminoAcid a, ost::conop::AminoAcid, Real dist);
 
@@ -44,9 +44,9 @@ public:
 
   Real GetTotalEnergy(ost::mol::EntityView& target, ost::mol::EntityView& env, bool normalize);
 
-  uint32_t GetCounts() { return count_; }
+  uint32_t GetCounts() const { return count_; }
 
-  impl::CBetaOpts GetOpts() { return opts_; }
+  impl::CBetaOpts GetOpts() const { return opts_; }
 
   template <typename DS>
   void Serialize(DS& ds){

src/cbeta_statistics.cc
@@ -45,7 +45,7 @@ void CBetaStatistic::Extract(ost::mol::EntityView& target, ost::mol::EntityView&
   target.Apply(*this);
 }
 
-Real CBetaStatistic::GetTotalCount(){
+Real CBetaStatistic::GetTotalCount() const{
   Real total_count=0.0;
   for(int i=0; i<opts_.number_of_bins; ++i){
     total_count+=this->GetCount(i);
@@ -53,11 +53,21 @@ Real CBetaStatistic::GetTotalCount(){
   return total_count;
 }
 
-Real CBetaStatistic::GetCount(ost::conop::AminoAcid a, ost::conop::AminoAcid b, int dist_bin){
+Real CBetaStatistic::GetCount(ost::conop::AminoAcid a, ost::conop::AminoAcid b, uint dist_bin) const{
+
+  if(a == ost::conop::XXX || b == ost::conop::XXX){
+    throw std::runtime_error("Cannot get count for invalid AminoAcid!");
+  }
+
+  if(dist_bin >= opts_.number_of_bins){
+    throw std::runtime_error("Cannot get count for invalid bin!");
+  }
+
   return histo_.Get(CBetaHistogram::IndexType(a, b, dist_bin)); 
 }
 
-Real CBetaStatistic::GetCount(ost::conop::AminoAcid a, ost::conop::AminoAcid b){
+Real CBetaStatistic::GetCount(ost::conop::AminoAcid a, ost::conop::AminoAcid b) const{
+
   Real count=0.0;
   for(int i=0; i<opts_.number_of_bins; ++i){
     count+=this->GetCount(a,b,i);
@@ -65,7 +75,8 @@ Real CBetaStatistic::GetCount(ost::conop::AminoAcid a, ost::conop::AminoAcid b){
   return count;
 }
 
-Real CBetaStatistic::GetCount(int dist_bin){
+Real CBetaStatistic::GetCount(uint dist_bin) const{
+
   Real count=0.0;
   for (size_t i=0; i<ost::conop::XXX; ++i) {
     for (size_t j=0; j<ost::conop::XXX; ++j) {

commit 85a7e0b2dc5918038c84cfb8004c04eae5cebb3f

undo bug introduced in 302180e2a835ba35a7069d7226f36c6e534e41e6

src/impl/cbeta_impl.cc
@@ -50,7 +50,7 @@ void CBetaPotentialImpl::SetEnvironment(ost::mol::EntityView& env){
   }
 }
 
-bool CBetaPotentialImpl::VisitResidue(ost::mol::ResidueHandle& res){
+bool CBetaPotentialImpl::VisitResidue(const ost::mol::ResidueHandle& res){
 
   if (!res.IsPeptideLinking()) {
     return false;

src/impl/cbeta_impl.hh
@@ -81,7 +81,7 @@ public:
     opts_(opts), env_(5) { }
 
 
-  virtual bool VisitResidue(ost::mol::ResidueHandle& res);
+  virtual bool VisitResidue(const ost::mol::ResidueHandle& res);
 
   virtual void OnInteraction(ost::conop::AminoAcid a,
                              ost::conop::AminoAcid b,