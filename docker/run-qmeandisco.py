89f25e383dc6004d16faac790b241d10d602992a
Stefan Bienert <stefan.bienert@unibas.ch>
2020-03-06 17:26:21 +0100

    First version of QMEAN DisCo doing 'something'

--- docker/run-qmeandisco.py
+++ docker/run-qmeandisco.py
@@ -1,35 +1,17 @@
-"""usage: sm run-qmeandisco <project>
+"""Run QMEAN DisCo for a mmCIF file. Writes to the same location as where the
+file comes from with '.json' as suffix.
 """
-import re
 import argparse
 import os
-import sys
 import json
+from datetime import datetime
 
 from qmean import QMEANScorer
-from qmean import AssessMembraneModelQuality
-from qmean import PSIPREDHandler
-from qmean import ACCPROHandler
 from qmean import predicted_sequence_features
-from qmean import DisCoContainer
-from qmean import mqa_result_membrane
 
 import ost
-from ost import io, conop
 from ost.io import LoadMMCIF
-from ost.bindings import hhblits
-from ost.bindings import WrappedTMAlign
-from ost.seq import CreateSequenceList
-from ost.mol.alg import FindMembrane, MolckSettings, Molck
 from ost.table import *
-from ost import LogWarning
-
-#from sm.core import cmptcmpt
-#from sm.pipeline import ProjectFactory
-#from sm.pipeline import RunHHblits
-#from sm.pipeline import CreateDistanceConstraints
-#from sm import config
-#from sm.meld.qa import FetchScoresFromScorer
 
 def _ParseArgs():
     parser = argparse.ArgumentParser(formatter_class=\
@@ -38,55 +20,47 @@ def _ParseArgs():
     parser.add_argument("model", help="Model file")
     parser.add_argument("-v", "--verbose", dest="verbose_flag",
                       default=0, help="Print more detailed output (0/1)")
+    parser.add_argument("-o", "--output",
+                        help="Path to write the JSON data to.")
 
     opts = parser.parse_args()
 
     return opts
 
+def _TurnIntoQMEANServerJSON(scores_json, ost_ent, seqres, file_nm):
+    result_json = {
+        #"2019-03-07T15:33:12.932"
+        # 2020-03-06 15:53:48.925243
+        'created':"{:%Y-%m-%dT%H:%M:%S}".format(datetime.now()),
+        'models':dict(),
+        'method':"QMEANDisCo",
+        # This needs to be fixed once we have the data in for doing DisCo
+        # sequence searches!
+        'smtl_version':"2020-03-04",
+        'seqres_uploaded': None,
+    }
 
-def _GetSequenceFeatures(proj):
+    # get per model scores
+    mdl_for_json = {
+        'original_name':file_nm,
+        'scores':scores_json,# scores: create deep copy of entity, let the renumbered copy be equipped with bfactors, run along residues, fetch befactor from renumbered entity and resnum/ chain name from deep copy to undo renumbering
+        'chains':dict()
+    }
 
-    seqres = dict() 
-    psipred_handler = dict()
-    accpro_handler = dict()
-    dc_container = dict()
+    for seq in seqres:
+        mdl_for_json['chains'][seq.name] = {
+            'seqres':str(seq),
+            #    'atomseq':seq['atomseq'], no example found yet, no idea if we need to set '-' for gaps
+            'name':seq.name
+        }
     
-    for subproject_name, subproject in proj.subprojects.items():
+    if not ost_ent.GetName():
+        result_json['models']["model_001"] = mdl_for_json
+    else:
+        result_json['models'][ost_ent.GetName()] = mdl_for_json
 
-        seqres[subproject_name] = subproject.target_seq
 
-        a3m_filename = subproject.a3m_filename
-        acc_filename = subproject.acc_filename
-        dc_filename = subproject.qmean_dc_filename
-
-        # try to get everything for PSIPREDHandler
-        if a3m_filename:
-            a3m_content = hhblits.ParseA3M(open(a3m_filename))
-            data = dict()
-            data['ss'] = a3m_content['ss_pred']
-            data['conf'] = [int(c) for c in a3m_content['ss_conf']]
-            data['seq'] = str(subproject.target_seq)
-            psipred_handler[subproject_name] = PSIPREDHandler(data)
-        else:
-            psipred_handler[subproject_name] = None
-
-        # try to get everything for ACCPROHandler
-        if acc_filename:
-            data = dict()
-            data["acc"] = str(io.LoadSequence(acc_filename))
-            data["seq"] = str(subproject.target_seq)
-            accpro_handler[subproject_name] = ACCPROHandler(data)
-        else:
-            accpro_handler[subproject_name] = None
-
-        # try to get everything for DisCoContainer
-        if dc_filename:
-            dc_container[subproject_name] =  \
-            DisCoContainer.Load(dc_filename)
-        else:
-            dc_container[subproject_name] = None
-
-    return (seqres, psipred_handler, accpro_handler, dc_container)
+    return result_json
 
 def _RenumberModel(model, seqres_list):
     ed = model.handle.EditXCS()
@@ -103,56 +77,6 @@ def _RenumberModel(model, seqres_list):
                 ed.SetResidueNumber(res.handle, new_num)
 
     return model
-
-
-def _SuperposeOnFirstChains(models):
-    # models already require chain names to be set
-    
-    chain_lengths = [len(m.chains[0].residues) for m in models]
-    reference_idx = chain_lengths.index(max(chain_lengths))
-    reference_view = models[reference_idx].CreateFullView()
-
-    for idx in range(len(models)):
-        if idx == reference_idx:
-            continue
-        model_view = models[idx].CreateFullView()
-        try:
-            tm_result = WrappedTMAlign(model_view.chains[0], reference_view.chains[0])
-            models[idx].handle.EditXCS().ApplyTransform(tm_result.transform)  
-        except:
-            pass
-
-    return models
-
-
-def _LoadRawModels(in_dir, model_files):
-    models = list()
-
-    for m_f in model_files:
-        try:
-            model = LoadPDB(
-                os.path.join(in_dir, m_f)).Select("peptide=true")
-            models.append(model)
-        except:
-            models.append(None)
-
-    return models
-
-
-def _LoadProcessedModels(in_dir, model_files, seqres_lists, proj):
-    models = list()
-
-    for m_f, s_l in zip(model_files, seqres_lists):
-        try:
-            model = LoadPDB(os.path.join(in_dir, m_f)).Select("peptide=true")
-            model = _RenumberModel(model, s_l)
-            models.append(model)
-        except Exception as ex:
-            models.append(None)
-
-    return_list = _SuperposeOnFirstChains(models)
-
-    return return_list
 
 def _AssembleResultJSON(scorer, seqres):
   """
@@ -209,7 +133,7 @@ def _AssembleResultJSON(scorer, seqres):
   try:
     global_scores["qmean6_norm_score"] = nan_to_none(scorer.qmean6_score)
     global_scores["qmean6_z_score"] = nan_to_none(scorer.qmean6_z_score)
-  except:
+  except Exception:
     global_scores["qmean6_norm_score"] = None
     global_scores["qmean6_z_score"] = None
 
@@ -230,138 +154,19 @@ def _AssembleResultJSON(scorer, seqres):
   global_scores["avg_local_score_error"] = nan_to_none(scorer.avg_local_score_error)
 
   local_scores = dict()
-  for ch, s in zip(scorer.model.chains, seqres):
-    score_list = list([None] * len(s))
-    per_chain_scores = scorer.local_scores[ch.GetName()]
+  for chn, seq in zip(scorer.model.chains, seqres):
+    score_list = list([None] * len(seq))
+    per_chain_scores = scorer.local_scores[chn.GetName()]
     for rnum, score in per_chain_scores.items():
-      if rnum < 1 or rnum > len(s):
+      if rnum < 1 or rnum > len(seq):
         raise RuntimeError("Observed ResNum not matching provided SEQRES!")
       score_list[rnum-1] = nan_to_none(score)
-    local_scores[ch.GetName()] = score_list
+    local_scores[chn.GetName()] = score_list
 
   result = dict()
   result["global_scores"] = global_scores
   result["local_scores"] = local_scores
   return result
-
-
-#proj = ProjectFactory.FromName(sys.argv[1])
-
-#opts, args = _ParseArgs(sys.argv[2:])
-
-#in_dir = proj.input_dir
-#out_dir = proj.results_dir
-#tmp_dir = config.QMEANSERVER_TMP_DIR
-
-## lets read the json stuff to get all required information about the project
-#project_data = proj.GetProjectJSON()
-
-## The actual action starts here, let's put everything in
-## a try block to perform appropriate error handling
-
-#try:
-
-#    # in case of QMEANDisCo we need a full fletched template search before
-#    # obtaining the distance constraints. In case of QMEAN or QMEANBrane
-#    # a sequence annotation is sufficient (aacpro and the profile from which)
-#    # to read the psipred data
-
-#    job = RunHHblits(proj, wait=True, use='PROCESS')
-#    if not job.completed:
-#        raise RuntimeError("HHBlits was not successful!")
-
-#    job = CreateDistanceConstraints(proj, wait=True, use='PROCESS')
-#    if not job.completed:
-#        raise RuntimeError("CreateDistanceConstraints was not successful!")
-
-#    # let's fetch the psipred handlers, accpro handlers and disco containers of
-#    # the full project
-#    full_seqres, full_psipred_handler, full_accpro_handler, full_dc_container =\
-#    _GetSequenceFeatures(proj)
-
-#    # lets extract the names of the models and the seqres sequences
-#    model_files = list()
-#    model_names = list()
-#    seqres_lists = list()
-#    out_paths = list()
-
-#    for item in project_data["models"]:
-#        model_files.append(str(item["modelid"]) + ".pdb")
-#        model_names.append(str(item["name"]))
-#        seq_list = CreateSequenceList()
-#        for s in item["seqres"]:
-#            seq_list.AddSequence(
-#                ost.seq.CreateSequence(str(s["name"]), str(s["sequence"])))
-#        seqres_lists.append(seq_list)
-
-#    # all models get loaded prior to any processing.
-#    raw_models = _LoadRawModels(in_dir, model_files)
-#    processed_models = _LoadProcessedModels(in_dir, model_files, seqres_lists, proj)
-
-#    # iterate over all models and do the quality assessment
-#    for mdl_idx in range(len(model_files)):
-
-#        model = processed_models[mdl_idx]
-#        raw_model = raw_models[mdl_idx]
-
-#        out_path = os.path.join(out_dir, model_files[mdl_idx].split('.')[0])
-#        out_paths.append(out_path)
-#        if not os.path.exists(out_path):
-#            os.makedirs(out_path)
-
-#        # from now on things could potentially go wrong, 
-#        # so we create a per model status file
-#        per_model_status_path = os.path.join(out_path, 'status')
-
-#        # set status to running
-#        _WriteModelStatus(per_model_status_path, "RUNNING")
-
-#        try:
-
-#            # find the model specific psipred / accpro and disco information
-
-#            psipred_handler = list()
-#            accpro_handler = list()
-#            dc_container = list()
-
-#            for seqres in seqres_lists[mdl_idx]:
-#                seqres_name = None
-#                for k,v in full_seqres.items():
-#                    if v.GetString() == seqres.GetString():
-#                        seqres_name = k
-#                        break
-#                if seqres_name == None:
-#                    raise RuntimeError("Could not find model seqres in \
-#                                        preprocessed sequences!")
-
-#                psipred_handler.append(full_psipred_handler[seqres_name])
-#                accpro_handler.append(full_accpro_handler[seqres_name])
-#                dc_container.append(full_dc_container[seqres_name])
-
-#            qmean_scorer = QMEANScorer(model, psipred=psipred_handler,
-#                                       accpro=accpro_handler, dc=dc_container,
-#                                       use_nn=True)
-#            qmean_scorer.AssignModelBFactors()
-#            score_dict = FetchScoresFromScorer(qmean_scorer, 
-#                                               seqres_lists[mdl_idx],
-#                                               assign_avg_local_score_error=True)
-#            outfile = open(os.path.join(out_path, "score_file.json"), 'w')
-#            json.dump(score_dict, outfile, indent=2, sort_keys=True)
-#            outfile.close()
-
-#            SavePDB(model, os.path.join(out_path, "model_processed.pdb"))
-
-#            # map over bfactors from processed model to rawmodel
-#            for r_processed, r_raw in zip(model.residues, raw_model.residues):
-#                bfac = r_processed.atoms[0].GetBFactor()
-#                for a in r_raw.atoms:
-#                    a.SetBFactor(bfac)
-
-#        except:
-#            raise
-
-#except Exception as e:                               
-#    raise
 
 def _main():
     """Run QMEAN DisCO
@@ -376,9 +181,18 @@ def _main():
                                accpro=None, dc=None,
                                use_nn=True)
 
-    result_json = _AssembleResultJSON(qmean_scorer, mdl_seq)
+    scores_json = _AssembleResultJSON(qmean_scorer, mdl_seq)
 
-    print(result_json)
+    result_json = _TurnIntoQMEANServerJSON(scores_json, mdl_ent, mdl_seq,
+                                           os.path.basename(opts.model))
+
+    if not opts.output:
+        out_path = "%s.json" % os.path.splitext(opts.model)[0]
+    else:
+        out_path = opts.output
+
+    with open(out_path, 'w') as ofh:
+        json.dump(result_json, ofh)#For pretty formatted files: indent=2, sort_keys=True)
 
 if __name__ == "__main__":
     _main()
