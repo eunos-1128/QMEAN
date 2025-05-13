2ffe8890d7e82e5796e9e600721733890456d094
UENO, M. <eunosium-1128@live.jp>
2025-05-13 15:06:26 +0000

    Fix formatting and include correct boost filesystem header in potential_base.hh and statistic_base.hh

--- src/potential_base.hh
+++ src/potential_base.hh
@@ -1,12 +1,12 @@
 // Copyright (c) 2013-2020, SIB - Swiss Institute of Bioinformatics and
 // Biozentrum - University of Basel
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 // http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -24,7 +24,7 @@
 #include <ost/io/binary_data_source.hh>
 #include <ost/io/binary_data_sink.hh>
 #include <ost/io/container_serialization.hh>
-#include <boost/filesystem/convenience.hpp>
+#include <boost/filesystem.hpp>
 #include <boost/pointer_cast.hpp>
 #include <ost/io/io_exception.hh>
 #include <limits>
--- src/statistic_base.hh
+++ src/statistic_base.hh
@@ -1,12 +1,12 @@
 // Copyright (c) 2013-2020, SIB - Swiss Institute of Bioinformatics and
 // Biozentrum - University of Basel
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 // http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,7 +23,7 @@
 #include <ost/io/container_serialization.hh>
 #include <boost/shared_ptr.hpp>
 #include <ost/io/io_exception.hh>
-#include <boost/filesystem/convenience.hpp>
+#include <boost/filesystem.hpp>
 #include <ost/log.hh>
 #include <qmean/float_histogram.hh>
 #include <vector>
@@ -63,7 +63,7 @@ public:
 
   StatisticBasePtr& operator[] (const String& key) { return statistics_[key]; }
 
-private: 
+private:
 
   std::map<String,StatisticBasePtr> statistics_;
 };
